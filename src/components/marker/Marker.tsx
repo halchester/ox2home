54ab172796488231a84573bb35b2ca5d420dcd16
nayyaung9 <naygit.developer@gmail.com>
2021-07-11 13:18:57 +0630

    style: modify  marker design and map style

--- src/components/marker/Marker.tsx
+++ src/components/marker/Marker.tsx
@@ -1,19 +1,13 @@
 import React from "react";
 //@ts-ignore
 import { Marker } from "react-map-gl";
+import "./marker.css";
 
 const MarkerPoint: React.FC<MarkerProps> = ({ item, onClickOnMarker }) => {
   return (
     <Marker latitude={item.latitude} longitude={item.longitude}>
       <button className="marker-btn" onClick={(e) => onClickOnMarker(e, item)}>
-        <img
-          src="/assets/images/o2-icon.png"
-          alt="o2-icon"
-          style={{
-            width: 30,
-            height: 30,
-          }}
-        />
+        <div className="circle" />
       </button>
     </Marker>
   );
--- src/components/popup/Popup.tsx
+++ src/components/popup/Popup.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 //@ts-ignore
 import { Popup } from "react-map-gl";
-import { Heading, Text } from "@chakra-ui/react";
+import { Heading, Text, Grid, GridItem, IconButton } from "@chakra-ui/react";
+import { PhoneIcon } from "@chakra-ui/icons";
+
+import "./popup.css";
 
 const PopupBox: React.FC<PopupProps> = ({ activePoint, setActivePoint }) => {
   return (
@@ -13,17 +16,29 @@ const PopupBox: React.FC<PopupProps> = ({ activePoint, setActivePoint }) => {
       onClose={() => {
         setActivePoint(null);
       }}
-      offsetLeft={10}
     >
       <Heading as="h5" size="sm" mb="3">
         {activePoint.title}
       </Heading>
-      <Text fontSize="md" mb="2">{activePoint.address}</Text>
+      <Text fontSize="md" mb="2">
+        {activePoint.address}
+      </Text>
       {activePoint.numbers.map((num: string, i: number) => (
-        <span key={i}>
-          {num}
-          <br />
-        </span>
+        <Grid templateColumns="repeat(3, 1fr)" gap={4} mb={2}>
+          <GridItem colSpan={2} alignSelf="center">
+            <Text fontSize="md">{num}</Text>
+          </GridItem>
+          <GridItem colStart={4} h="10">
+            <IconButton
+              colorScheme="teal"
+              aria-label="Call Service"
+              size="sm"
+              isRound
+              onClick={() => window.open(`tel:${num}`)}
+              icon={<PhoneIcon />}
+            />
+          </GridItem>
+        </Grid>
       ))}
     </Popup>
   );
